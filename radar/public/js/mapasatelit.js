0749798dd2e59ddedd5ccb8094c5250324636e8c
T3mpest_.strike <simocorreos@gmail.com>
2026-08-31 14:27:23 +0200

    canvi

--- radar/public/js/mapasatelit.js
+++ radar/public/js/mapasatelit.js
@@ -22,7 +22,7 @@ const CONFIG = {
 
   initialCenter: [40.5, 0.0],
   initialZoom: 7,
-  defaultOpacity: 1.0,  // Opacidad máxima para mejor visualización
+  defaultOpacity: 75,  // Opacidad máxima para mejor visualización
 };
 
 const AUTO_REFRESH_MS = 5 * 60 * 1000; // 5 minuts
